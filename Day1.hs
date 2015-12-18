26be391818b82151f415424cb3220759d7d063b2
Aleksey <astynax84@gmail.com>
2015-12-18 12:14:46 +0300

    init, add solution for Day #1

---
+++ Day1.hs
@@ -0,0 +1,40 @@
+module Day1 where
+
+import Data.List (foldl')
+import Data.Maybe (catMaybes)
+
+data Move  = Up | Down
+
+type Pos   = Int
+type Floor = Int
+
+main :: IO ()
+main = readFile "inputs/day1.txt" >>= render . solve . parse
+
+parse :: String -> [Move]
+parse = catMaybes . map toMove
+  where
+    toMove '(' = Just Up
+    toMove ')' = Just Down
+    toMove _   = Nothing
+
+solve :: [Move] -> (Pos, Floor)
+solve = foldl' go (-1, 0) . zip [1..]
+  where
+    go (pos, floor') (cnt, move) =
+      let nextFloor = floor' + case move of
+            Up -> 1
+            Down -> negate 1
+      in  ( if pos >= 0
+            then pos
+            else
+              if nextFloor < 0
+              then cnt
+              else -1
+          , nextFloor
+          )
+
+render :: (Pos, Floor) -> IO ()
+render (p, f) =
+  putStrLn "Pos:" >> print p
+  >> putStrLn "Floor:" >> print f
