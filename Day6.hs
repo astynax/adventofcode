23259ebd804dc574756ba6fe4209a5dd6613780c
Aleksey Pirogov <astynax84@gmail.com>
2015-12-19 20:50:26 +0300

    add solution for the Day #6

---
+++ Day6.hs
@@ -0,0 +1,103 @@
+module Day6 where
+
+import Data.List (foldl')
+import Data.Maybe (fromMaybe)
+import Data.Map.Strict (Map, fromList, mergeWithKey, empty, size)
+
+data State  = On | Off deriving Eq
+data Action = Turn State | Toggle deriving Eq
+
+type Coords   = (Int, Int)
+type Grid     = Map Coords ()
+
+type Input    = (Action, Coords, Coords)
+type Solution = Int
+
+
+main :: IO ()
+main = readFile "inputs/day6.txt" >>= render . solve . parse
+
+
+parse :: String -> [Input]
+parse = map parseOne . lines
+  where
+    parseOne i =
+      let badInput = error $ "Bad input: " ++ i
+          (c1, c2, act) = case words i of
+            ["toggle",      f, "through", t] -> (f, t, Toggle)
+            ["turn", "on",  f, "through", t] -> (f, t, Turn On)
+            ["turn", "off", f, "through", t] -> (f, t, Turn Off)
+            _ -> badInput
+      in  (act, parseCoords c1, parseCoords c2)
+
+    parseCoords :: String -> Coords
+    parseCoords s =
+      fromMaybe (error $ "Bad coord:" ++ s)
+      $ case reads s of
+        [(x, ',' : s')] | inRange x ->
+          case reads s' of
+            [(y, "")] | inRange y ->
+              Just (x, y)
+            _ -> Nothing
+        _ -> Nothing
+
+    inRange x = x >= 0 && x < 1000
+
+
+solve :: [Input] -> Solution
+solve = size . foldl' f empty
+  where
+    f g (act, c1, c2) = update g act (area c1 c2)
+
+
+render :: Solution -> IO ()
+render = print
+
+-- helpers
+
+update :: Grid -> Action -> [Coords] -> Grid
+update g act cs =
+  let (merge, apply) = case act of
+        Turn On  -> ( keepOld,   keepNew )
+        Turn Off -> ( rejectOld, rejectNew )
+        Toggle   -> ( rejectOld, keepNew )
+  in  mergeWithKey merge id apply g patch
+  where
+    keepOld   _ _ _ = Just ()
+    rejectOld _ _ _ = Nothing
+    keepNew         = id
+    rejectNew       = const empty
+
+    patch = fromList $ zip cs $ repeat ()
+
+area :: Coords -> Coords -> [Coords]
+area (x1, y1) (x2, y2) =
+  [ (x, y)
+  | y <- [min y1 y2 .. max y1 y2]
+  , x <- [min x1 x2 .. max x1 x2]
+  ]
+
+-- selfcheck
+
+worksFine :: Bool
+worksFine = parseWorksFine && updateWorksFine
+
+
+parseWorksFine :: Bool
+parseWorksFine =
+  parse ("turn off 0,0 through 1,1\n" ++
+         "toggle 2,4 through 4,2\n" ++
+         "turn on 100,100 through 900,900"
+        ) == [ (Turn Off, (0,   0),   (1,   1))
+             , (Toggle,   (2,   4),   (4,   2))
+             , (Turn On,  (100, 100), (900, 900)) ]
+
+
+updateWorksFine :: Bool
+updateWorksFine =
+  update    src Toggle     [(0, 0), (1, 0)] == grid [(1, 0), (1, 1)]
+  && update src (Turn Off) [(0, 0), (1, 0)] == grid [(1, 1)]
+  && update src (Turn On)  [(0, 0), (1, 0)] == grid [(0, 0), (1, 0), (1, 1)]
+  where
+    src = grid [(0, 0), (1, 1)]
+    grid = fromList . flip zip (repeat ())
---
+++ Day6_2.hs
@@ -0,0 +1,52 @@
+module Day6_2 where
+
+import Day6 hiding (main, solve, update, Grid)
+
+import Data.List (foldl')
+import Data.Map.Strict (Map, fromList, mergeWithKey, empty, toList)
+
+
+type Grid = Map Coords Int
+
+
+main :: IO ()
+main = readFile "inputs/day6.txt" >>= render . solve . parse
+
+
+solve :: [Input] -> Solution
+solve = sum . map snd . toList . foldl' f empty
+  where
+    f g (act, c1, c2) = update g act (area c1 c2)
+
+
+-- helpers
+
+update :: Grid -> Action -> [Coords] -> Grid
+update g act cs =
+  let (merge, apply, patchValue) = case act of
+        Toggle   ->
+          (\_ old _ -> Just (old + 2),         keepNew,   2)
+        Turn On  ->
+          (\_ old _ -> Just (old + 1),         keepNew,   1)
+        Turn Off ->
+          (\_ old _ -> Just (max (old - 1) 0), rejectNew, 0)
+  in  mergeWithKey merge id apply g (mkPatch patchValue)
+  where
+    keepNew         = id
+    rejectNew       = const empty
+
+    mkPatch = fromList . zip cs . repeat
+
+-- selfcheck
+
+worksFine :: Bool
+worksFine =
+  let g1 = update empty (Turn On)  [(0, 0), (1, 1)]
+      g2 = update g1    Toggle     [(1, 1), (2, 2), (3, 3)]
+      g3 = update g2    (Turn Off) [(0, 0), (2, 2)]
+      g4 = update g3    (Turn Off) [(0, 0)]
+  in  g4 == fromList [ ((0, 0), 0)
+                     , ((1, 1), 3)
+                     , ((2, 2), 1)
+                     , ((3, 3), 2)
+                     ]
